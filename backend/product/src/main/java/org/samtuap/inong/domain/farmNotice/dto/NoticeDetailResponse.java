6770a2e7b95423afefdb5cd9eaf8f4a28bca0d2d
H-Yeji <yeji980603@naver.com>
2024-09-17 14:59:47 +0900

    chore: 농장의 공지 조회 기능 관련 엔티티 및 리턴 수정

--- backend/product/src/main/java/org/samtuap/inong/domain/farmNotice/dto/NoticeDetailResponse.java
+++ backend/product/src/main/java/org/samtuap/inong/domain/farmNotice/dto/NoticeDetailResponse.java
@@ -1,35 +1,30 @@
 package org.samtuap.inong.domain.farmNotice.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.samtuap.inong.domain.farmNotice.entity.FarmNotice;
 import org.samtuap.inong.domain.farmNotice.entity.FarmNoticeImage;
 
 import java.util.List;
 
-@Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class NoticeDetailResponse {
-
-    private Long id;
-
-    private String title;
-
-    private String content;
-
-    // 공지 이미지 담을 리스트
-    private List<FarmNoticeImage> noticeImages;
+public record NoticeDetailResponse(
+        Long id,
+        String title,
+        String content,
+        List<String> noticeImages
+) {
 
     public static NoticeDetailResponse from(FarmNotice notice, List<FarmNoticeImage> noticeImages) {
+
+        List<String> imageUrls = noticeImages.stream()
+                .map(FarmNoticeImage::getImageUrl)
+                .toList();
+
         return NoticeDetailResponse.builder()
                 .id(notice.getId())
                 .title(notice.getTitle())
                 .content(notice.getContents())
-                .noticeImages(noticeImages)
+                .noticeImages(imageUrls)
                 .build();
     }
 }
--- backend/product/src/main/java/org/samtuap/inong/domain/farmNotice/dto/NoticeListResponse.java
+++ backend/product/src/main/java/org/samtuap/inong/domain/farmNotice/dto/NoticeListResponse.java
@@ -1,36 +1,30 @@
 package org.samtuap.inong.domain.farmNotice.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.samtuap.inong.domain.farmNotice.entity.FarmNotice;
 import org.samtuap.inong.domain.farmNotice.entity.FarmNoticeImage;
 
 import java.util.List;
 
-@Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class NoticeListResponse {
-
-    private Long id;
-
-    private String title;
-
-    private String content;
-
-    // 공지 이미지 담을 리스트
-    private List<FarmNoticeImage> noticeImages;
+public record NoticeListResponse(
+        Long id,
+        String title,
+        String content,
+        List<String> noticeImages
+) {
 
     public static NoticeListResponse from(FarmNotice notice, List<FarmNoticeImage> noticeImages) {
+
+        List<String> imageUrls = noticeImages.stream()
+                .map(FarmNoticeImage::getImageUrl)
+                .toList();
+
         return NoticeListResponse.builder()
                 .id(notice.getId())
                 .title(notice.getTitle())
                 .content(notice.getContents())
-                .noticeImages(noticeImages)
+                .noticeImages(imageUrls)
                 .build();
     }
-
 }
