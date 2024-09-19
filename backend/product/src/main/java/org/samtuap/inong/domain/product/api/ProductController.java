2f4e8efa252e9f20afc32e978c95d5e3df357263
Sejeong Kim <clearrworld@gmail.com>
2024-09-19 12:34:56 +0900

    chore: 에러 테스트 api 생성

--- backend/product/src/main/java/org/samtuap/inong/domain/product/api/ProductController.java
+++ backend/product/src/main/java/org/samtuap/inong/domain/product/api/ProductController.java
@@ -1,8 +1,12 @@
 package org.samtuap.inong.domain.product.api;
 
+import org.samtuap.inong.common.exception.BaseCustomException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.samtuap.inong.common.exceptionType.ProductExceptionType.PRODUCT_NOT_FOUND;
+
 
 @RequestMapping("/product")
 @RestController
@@ -10,5 +14,10 @@ public class ProductController {
     @GetMapping
     public String testApi() {
         return "product-test!!";
+    }
+
+    @GetMapping("/test")
+    public void exceptionTest() {
+        throw new BaseCustomException(PRODUCT_NOT_FOUND);
     }
 }
