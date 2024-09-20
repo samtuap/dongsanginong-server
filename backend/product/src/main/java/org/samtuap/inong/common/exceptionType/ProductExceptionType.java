446e4b6274390f06170d8e72e59b97b59c647e50
H-Yeji <yeji980603@naver.com>
2024-09-20 14:42:11 +0900

    chore: merge 후 수정사항 업데이트

--- backend/product/src/main/java/org/samtuap/inong/common/exceptionType/ProductExceptionType.java
+++ backend/product/src/main/java/org/samtuap/inong/common/exceptionType/ProductExceptionType.java
@@ -10,7 +10,7 @@ public enum ProductExceptionType implements ExceptionType {
     NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 농장에 해당하는 공지사항이 존재하지 않습니다."),
     FIELD_NOT_FOUND(HttpStatus.NOT_FOUND, "필드를 업데이트 할 수 없습니다."),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하는 이메일이 없습니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 Id의 댓글이 존재하지 않습니다."),
     UNAUTHORIZED_ACTION(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다.");
 
