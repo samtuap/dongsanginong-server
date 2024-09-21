25429c39ae70ca5bf282cfb5936f65ce3b4adec8
Sejeong Kim <clearrworld@gmail.com>
2024-09-21 20:34:09 +0900

    feat: GlobalExceptionHandler에 바인딩 에러 추가

--- backend/member/src/main/java/org/samtuap/inong/domain/notification/api/FcmController.java
+++ backend/member/src/main/java/org/samtuap/inong/domain/notification/api/FcmController.java
@@ -1,10 +1,12 @@
 package org.samtuap.inong.domain.notification.api;
 
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.samtuap.inong.domain.notification.dto.FcmTokenSaveRequest;
 import org.samtuap.inong.domain.notification.service.FcmService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,7 +16,7 @@ public class FcmController {
     private final FcmService fcmService;
     @PostMapping("/token")
     public ResponseEntity<Void> saveFcmToken(@RequestHeader Long myId,
-                                             @RequestBody FcmTokenSaveRequest fcmTokenSaveRequest) {
+                                             @RequestBody @Valid FcmTokenSaveRequest fcmTokenSaveRequest) {
         fcmService.saveFcmToken(myId, fcmTokenSaveRequest);
         return ResponseEntity.ok(null);
     }
--- backend/member/src/main/java/org/samtuap/inong/domain/notification/repository/NotificationRepository.java
+++ backend/member/src/main/java/org/samtuap/inong/domain/notification/repository/NotificationRepository.java
@@ -3,5 +3,5 @@ package org.samtuap.inong.domain.notification.repository;
 import org.samtuap.inong.domain.favorites.entity.Favorites;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
+public interface NotificationRepository extends JpaRepository<Favorites, Long> {
 }
