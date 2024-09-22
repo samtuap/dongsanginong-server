21627c9868b0a4afb31f35f527dda9df4eb405f6
vydndrl <vydndrl1234@naver.com>
2024-09-22 11:54:33 +0900

    repository 충돌 해결

--- backend/product/src/main/java/org/samtuap/inong/domain/product/repository/PackageProductRepository.java
+++ backend/product/src/main/java/org/samtuap/inong/domain/product/repository/PackageProductRepository.java
@@ -10,19 +10,16 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-import static org.samtuap.inong.common.exceptionType.ProductExceptionType.*;
+import static org.samtuap.inong.common.exceptionType.ProductExceptionType.PRODUCT_NOT_FOUND;
 
 public interface PackageProductRepository extends JpaRepository<PackageProduct, Long> {
     @Query("SELECT p FROM PackageProduct p WHERE p.id IN :ids")
     List<PackageProduct> findAllByIds(@Param("ids") List<Long> ids);
 
-
     @Query("SELECT p FROM PackageProduct p WHERE p.farm.sellerId = :sellerId")
     Page<PackageProduct> findBySellerId(@Param("sellerId") Long sellerId, Pageable pageable);
-}
 
     default PackageProduct findByIdOrThrow(Long packageId){
         return findById(packageId).orElseThrow(()->new BaseCustomException(PRODUCT_NOT_FOUND));
     }
 }
-
