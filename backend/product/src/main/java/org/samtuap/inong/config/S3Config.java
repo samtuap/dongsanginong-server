f496529c8d3dd512103d9b8e81c7a50b9c84d93a
vydndrl <vydndrl1234@naver.com>
2024-09-27 11:49:39 +0900

    chore: presignedUrl 발급 과정 수정

---
+++ backend/product/src/main/java/org/samtuap/inong/config/S3Config.java
@@ -0,0 +1,41 @@
+package org.samtuap.inong.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Slf4j
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    @Primary
+    public BasicAWSCredentials awsCredentialsProvider(){
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return basicAWSCredentials;
+    }
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        log.info("accesskey: {}", accessKey);
+        log.info("secret key: {}", secretKey);
+        log.info("region: {}", region);
+        AmazonS3 s3Builder = AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+                .build();
+        return s3Builder;
+    }
+}
