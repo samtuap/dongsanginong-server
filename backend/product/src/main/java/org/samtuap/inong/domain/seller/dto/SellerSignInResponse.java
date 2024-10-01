4b3ac79d0dd6c41563c9541c81848061d4f48a36
vydndrl <vydndrl1234@naver.com>
2024-10-01 10:23:05 +0900

    fix: response sellerId로 수정

--- backend/product/src/main/java/org/samtuap/inong/domain/seller/dto/SellerSignInResponse.java
+++ backend/product/src/main/java/org/samtuap/inong/domain/seller/dto/SellerSignInResponse.java
@@ -3,7 +3,7 @@ package org.samtuap.inong.domain.seller.dto;
 import org.samtuap.inong.domain.seller.jwt.domain.JwtToken;
 import org.samtuap.inong.domain.seller.entity.Seller;
 
-public record SellerSignInResponse(Long memberId,
+public record SellerSignInResponse(Long sellerId,
                                    String name,
                                    String email,
                                    String accessToken,
