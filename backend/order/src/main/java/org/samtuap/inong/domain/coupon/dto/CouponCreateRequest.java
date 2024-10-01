54c2ff5e9a4503529842f279a6875361897cb510
cogns <99cogns@naver.com>
2024-10-01 09:47:14 +0900

    chore: 쿠폰 expiration타입 변경

--- backend/order/src/main/java/org/samtuap/inong/domain/coupon/dto/CouponCreateRequest.java
+++ backend/order/src/main/java/org/samtuap/inong/domain/coupon/dto/CouponCreateRequest.java
@@ -3,19 +3,22 @@ package org.samtuap.inong.domain.coupon.dto;
 import jakarta.validation.constraints.NotNull;
 import org.samtuap.inong.domain.coupon.entity.Coupon;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public record CouponCreateRequest(
         @NotNull String couponName,
         @NotNull Integer discountPercentage,
-        @NotNull LocalDateTime expiration,
+        @NotNull LocalDate expirationDate,  // LocalDate로 날짜 받기
+        @NotNull LocalTime expirationTime,  // LocalTime으로 시간 받기
         @NotNull Long farmId
 ) {
     public static Coupon toEntity(CouponCreateRequest dto, Long farmId) {
+        // LocalDate와 LocalTime을 LocalDateTime으로 합치기
         return Coupon.builder()
                 .couponName(dto.couponName)
                 .discountPercentage(dto.discountPercentage)
-                .expiration(dto.expiration)
+                .expiration(dto.expirationDate.atTime(dto.expirationTime))  // LocalDateTime으로 변환
                 .farmId(farmId)
                 .build();
     }
