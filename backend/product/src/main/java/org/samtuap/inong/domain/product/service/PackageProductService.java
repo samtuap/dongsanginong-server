3645908b30978e14b7b045290cc98eb2d4b23fcd
vydndrl <vydndrl1234@naver.com>
2024-10-03 22:31:10 +0900

    chore: 패키지 관련 로직 수정

--- backend/product/src/main/java/org/samtuap/inong/domain/product/service/PackageProductService.java
+++ backend/product/src/main/java/org/samtuap/inong/domain/product/service/PackageProductService.java
@@ -16,7 +16,6 @@ import org.samtuap.inong.search.service.PackageProductSearchService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
