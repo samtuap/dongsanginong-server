c34823885b9e42b2f960b4ecc33bc1b54790737a
vydndrl <vydndrl1234@naver.com>
2024-10-10 11:18:07 +0900

    feat: 사용자 강퇴 기능 추가

--- backend/live/src/main/java/org/samtuap/inong/common/exceptionType/ChatExceptionType.java
+++ backend/live/src/main/java/org/samtuap/inong/common/exceptionType/ChatExceptionType.java
@@ -9,7 +9,8 @@ public enum ChatExceptionType implements ExceptionType {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 id의 회원이 존재하지 않습니다."),
     AUTHORIZATION_HEADER_MISSING(HttpStatus.UNAUTHORIZED, "Authorization 헤더가 없거나 유효하지 않습니다."),
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
-    FARM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당의 아이디 농장이이 존재하지 않습니다.");
+    FARM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당의 아이디 농장이이 존재하지 않습니다."),
+    IS_NOT_OWNER(HttpStatus.UNAUTHORIZED, "해당 사용자는 owner가 아닙니다.");
 
 
     private final HttpStatus status;
--- backend/live/src/main/java/org/samtuap/inong/domain/chat/service/ChatService.java
+++ backend/live/src/main/java/org/samtuap/inong/domain/chat/service/ChatService.java
@@ -4,15 +4,24 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.samtuap.inong.common.client.FarmFeign;
 import org.samtuap.inong.common.client.MemberFeign;
+import org.samtuap.inong.common.exception.BaseCustomException;
 import org.samtuap.inong.domain.chat.dto.ChatMessageRequest;
+import org.samtuap.inong.domain.chat.dto.KickMessage;
 import org.samtuap.inong.domain.chat.dto.MemberDetailResponse;
 import org.samtuap.inong.domain.chat.kafka.KafkaConstants;
 import org.samtuap.inong.domain.live.dto.FarmDetailGetResponse;
 import org.samtuap.inong.domain.live.entity.Live;
 import org.samtuap.inong.domain.live.repository.LiveRepository;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.samtuap.inong.common.exceptionType.ChatExceptionType.IS_NOT_OWNER;
+import static org.samtuap.inong.common.exceptionType.LiveExceptionType.LIVE_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +32,8 @@ public class ChatService {
     private final MemberFeign memberFeign;
     private final FarmFeign farmFeign;
     private final LiveRepository liveRepository;
+    private final ConcurrentHashMap<String, Set<Long>> kickedUsersMap = new ConcurrentHashMap<>();
+    private final SimpMessagingTemplate messagingTemplate;
 
     public void processAndSendMessage(String sessionId, ChatMessageRequest messageRequest) {
         Long memberId = messageRequest.memberId();
@@ -32,48 +43,53 @@ public class ChatService {
         String senderName = messageRequest.name();
         boolean isOwner = false;
 
+        // 사용자가 강퇴된 상태인지 확인
+        if (isUserKicked(sessionId, memberId)) {
+            log.info("강퇴된 사용자 메시지 차단: sessionId = {}, memberId = {}", sessionId, memberId);
+            return;
+        }
+
         // 개설자인지 여부 확인
         try {
             Live live = liveRepository.findBySessionId(sessionId)
-                    .orElseThrow(() -> new IllegalArgumentException("Live not found for id: " + sessionId));
+                    .orElseThrow(() -> new BaseCustomException(LIVE_NOT_FOUND));
             if (live.getOwnerId().equals(sellerId)) {
                 isOwner = true;
             }
         } catch (Exception e) {
-            log.error("Error fetching live info for sessionId: {}", sessionId, e);
+            log.error("세션에 대한 라이브 정보 불러오기 오류: {}", sessionId, e);
         }
 
-        log.info("Sending message with name: {}, isOwner: {}", senderName, isOwner);
+        log.info("메시지 전송 name: {}, isOwner: {}", senderName, isOwner);
 
         // 멤버 ID가 있는 경우 멤버 이름 설정
         if (memberId != null) {
-            log.info("Fetching member info for memberId: {}", memberId);
+            log.info("멤버 정보 가져오기 memberId: {}", memberId);
             try {
                 MemberDetailResponse memberInfo = memberFeign.getMemberById(memberId);
                 if (memberInfo != null) {
                     senderName = memberInfo.name();  // 멤버 이름 사용
-                    log.info("Member name retrieved: {}", senderName);
                 } else {
-                    log.warn("Member not found for memberId: {}", memberId);
+                    log.warn("해당 멤버가 존재하지 않음 memberId: {}", memberId);
                 }
             } catch (Exception e) {
-                log.error("Error fetching member info for memberId: {}", memberId, e);
+                log.error("멤버 정보 가져오는데 오류: {}", memberId, e);
             }
         }
 
         // 판매자일 경우 농장 이름 설정 (isSeller 값은 수정하지 않음)
         if (sellerId != null) {
-            log.info("Fetching farm info for sellerId: {}", sellerId);
+            log.info("판매자 농장 정보 가져오기 sellerId: {}", sellerId);
             try {
                 FarmDetailGetResponse farmInfo = farmFeign.getFarmInfoWithSellerId(sellerId);
                 if (farmInfo != null) {
                     senderName = farmInfo.farmName();  // 판매자 이름 대신 농장 이름 사용
-                    log.info("Farm name retrieved: {}", senderName);
+                    log.info("농장 이름: {}", senderName);
                 } else {
-                    log.warn("Farm not found for sellerId: {}", sellerId);
+                    log.warn("해당 판매자의 농장이 없음: {}", sellerId);
                 }
             } catch (Exception e) {
-                log.error("Error fetching farm info for sellerId: {}", sellerId, e);
+                log.error("판매자의 농장 정보 가져오는데 오류: {}", sellerId, e);
             }
         }
 
@@ -90,5 +106,37 @@ public class ChatService {
 
         log.info("Sending message to Kafka topic: {}, message: {}", KafkaConstants.KAFKA_TOPIC, updatedRequest);
         kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, updatedRequest);
+    }
+
+    public void kickUser(String sessionId, Long memberId, Long sellerId) {
+        // 개설자인지 확인 (liveId로 소유자 확인 로직 추가 가능)
+        if (!isOwner(sessionId, sellerId)) {
+            throw new BaseCustomException(IS_NOT_OWNER);
+        }
+
+        // 강퇴된 사용자 추가
+        kickedUsersMap.computeIfAbsent(sessionId, k -> new HashSet<>()).add(memberId);
+        log.info("사용자 강퇴됨: sessionId = {}, memberId = {}", sessionId, memberId);
+
+        // 강퇴된 사용자에게 강퇴 메시지 전송
+        messagingTemplate.convertAndSend("/topic/kick/" + memberId, new KickMessage(memberId, "강퇴되었습니다."));
+    }
+
+    private boolean isOwner(String sessionId, Long sellerId) {
+        try {
+            Live live = liveRepository.findBySessionId(sessionId)
+                    .orElseThrow(() -> new BaseCustomException(LIVE_NOT_FOUND));
+
+            // ownerId와 sellerId 비교
+            return live.getOwnerId().equals(sellerId);
+        } catch (Exception e) {
+            log.error("owner 확인 중 오류 sessionId: {}, sellerId: {}", sessionId, sellerId, e);
+            return false; // 에러가 발생하면 소유자가 아닌 것으로 처리
+        }
+    }
+
+    // 강퇴된 사용자의 접속을 차단하기 위한 메서드
+    public boolean isUserKicked(String sessionId, Long memberId) {
+        return kickedUsersMap.containsKey(sessionId) && kickedUsersMap.get(sessionId).contains(memberId);
     }
 }
