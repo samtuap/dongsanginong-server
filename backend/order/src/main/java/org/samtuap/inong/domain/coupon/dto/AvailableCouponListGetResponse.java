fc152d1f51c127f4511606d560819608d1112667
Sejeong Kim <clearrworld@gmail.com>
2024-10-13 20:48:23 +0900

    feat: 주문시 적용 가능한 쿠폰 조회 API 구현

---
+++ backend/order/src/main/java/org/samtuap/inong/domain/coupon/dto/AvailableCouponListGetResponse.java
@@ -0,0 +1,6 @@
+package org.samtuap.inong.domain.coupon.dto;
+
+import java.util.List;
+
+public record AvailableCouponListGetResponse(List<AvailableCouponGetResponse> availableCouponList) {
+}
--- backend/order/src/main/java/org/samtuap/inong/domain/coupon/repository/MemberCouponRelationRepository.java
+++ backend/order/src/main/java/org/samtuap/inong/domain/coupon/repository/MemberCouponRelationRepository.java
@@ -3,6 +3,8 @@ package org.samtuap.inong.domain.coupon.repository;
 import org.samtuap.inong.domain.coupon.entity.Coupon;
 import org.samtuap.inong.domain.coupon.entity.MemberCouponRelation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +13,14 @@ import java.util.Optional;
 public interface MemberCouponRelationRepository extends JpaRepository<MemberCouponRelation, Long> {
     Optional<MemberCouponRelation> findByCouponIdAndMemberId(Long couponId, Long MemberId);
     boolean existsByCouponIdAndMemberId(Long couponId, long memberId);
-    List<MemberCouponRelation> findAllByMemberIdAndUsedAt(Long memberId, LocalDateTime usedAt);
+    List<MemberCouponRelation> findAllByMemberIdAndUsedAtIsNull(Long memberId);
+
+    @Query("SELECT mcr FROM MemberCouponRelation mcr" +
+            " JOIN FETCH mcr.coupon" +
+            " WHERE mcr.memberId = :memberId" +
+            " AND mcr.coupon.farmId = :farmId" +
+            " AND mcr.usedAt IS NULL")
+    List<MemberCouponRelation> findAllByMemberIdAndFarmIdAndUsedAtIsNull(@Param("memberId") Long memberId, @Param("farmId") Long farmId);
 
 
 }
