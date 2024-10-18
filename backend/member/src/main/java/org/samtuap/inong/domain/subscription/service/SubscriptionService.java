0300f15fb82aad54be0baca48034e743ff4bb723
Sejeong Kim <clearrworld@gmail.com>
2024-10-18 11:20:22 +0900

    chore: 롤백을 위한 Exception 코드 삭제

--- backend/member/src/main/java/org/samtuap/inong/domain/subscription/service/SubscriptionService.java
+++ backend/member/src/main/java/org/samtuap/inong/domain/subscription/service/SubscriptionService.java
@@ -109,7 +109,6 @@ public class SubscriptionService {
         try {
             subscribeRequest = objectMapper.readValue(message, KafkaSubscribeProductRequest.class);
             subscribePackageProduct(subscribeRequest);
-            throw new RuntimeException("!!!!!!");
         } catch (JsonProcessingException e) {
             throw new BaseCustomException(INVALID_SUBSCRIPTION_REQUEST);
         } catch(Exception e) {
