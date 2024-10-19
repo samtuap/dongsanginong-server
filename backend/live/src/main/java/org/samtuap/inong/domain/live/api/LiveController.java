6175c69d6d6b48d45aace337634dcf81bf3a2a3c
H-Yeji <yeji980603@naver.com>
2024-10-20 01:52:54 +0900

    feat: 메인 페이지 > 라이브 목록 시청순 best5 출력

--- backend/live/src/main/java/org/samtuap/inong/domain/live/api/LiveController.java
+++ backend/live/src/main/java/org/samtuap/inong/domain/live/api/LiveController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.samtuap.inong.domain.live.dto.FavoritesLiveListResponse;
 import org.samtuap.inong.domain.live.service.LiveService;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -17,7 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.samtuap.inong.domain.live.dto.ActiveLiveListGetResponse;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/live")
 @RequiredArgsConstructor
@@ -45,5 +48,23 @@ public class LiveController {
     public ResponseEntity<Integer> getParticipantCount(@PathVariable("sessionId") String sessionId) {
         int count = liveService.getParticipantCount(sessionId);
         return ResponseEntity.ok(count);
+    }
+
+    /**
+     * main page => 라이브 목록 시청자 순 best5
+     */
+    @GetMapping("/active/best")
+    public ResponseEntity<Page<ActiveLiveListGetResponse>> getBestLive(@PageableDefault(size = 5) Pageable pageable) {
+        Page<ActiveLiveListGetResponse> response = liveService.getActiveLiveList(pageable);
+
+        List<ActiveLiveListGetResponse> sortedList = response.stream()
+                .sorted(Comparator.comparing(ActiveLiveListGetResponse::participantCount).reversed())
+                .collect(Collectors.toList());
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), sortedList.size());
+        Page<ActiveLiveListGetResponse> sortedPage = new PageImpl<>(sortedList.subList(start, end), pageable, sortedList.size());
+
+        return new ResponseEntity<>(sortedPage, HttpStatus.OK);
     }
 }
